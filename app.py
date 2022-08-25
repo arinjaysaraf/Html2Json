e004150bf0655d26b2410ccc40a74cf4c924274e
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-08-25 17:49:34 +0530

    deploy

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
 from bs4 import BeautifulSoup
 import html
 import json
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
 
-soup = BeautifulSoup((open('index.html')), 'html.parser')
+class html2json(BaseModel):
+    html: str
 
-text = soup.find_all('p')
-img = soup.find_all('img')
-data = {}
-k=1
-src_=[]
+@app.get("/")
+def sendJson(data: html2json):
+    soup = BeautifulSoup(data.html, 'html.parser')
+    text = soup.find_all('p')
+    img = soup.find_all('img')
+    data = {}
+    k=1
+    for i in text:
+        data[str(k)] = str(i.get_text())
+        img = html.unescape(i).find_all('img')
+        for j in img:
+            if len(j['src']) > 0:
+                data[str(k)] = j['src']
+        k+=1
+    json_data = json.loads(json.dumps(soup))
+    return [{ "json_data": json_data}]
 
-codes = { 
-    "\u2019" : "'",
-    "\u2018" : "'",
-    "\u201c" : '"',
-    "\u201d" : '"',
-    "\u2013" : '-',
-    "\u2014" : '-',
-    "\u2026" : '...',
-    "\u00a0" : ' '
-}
+# soup = BeautifulSoup((open('index.html')), 'html.parser')
 
-for i in text:
-    data[str(k)] = str(i.get_text())
-    img = html.unescape(i).find_all('img')
-    for j in img:
-        if len(j['src']) > 0:
-            data[str(k)] = j['src']
-            src_ = j['src']
-    k+=1
+# text = soup.find_all('p')
+# img = soup.find_all('img')
+# data = {}
+# k=1
+# for i in text:
+#     data[str(k)] = str(i.get_text())
+#     img = html.unescape(i).find_all('img')
+#     for j in img:
+#         if len(j['src']) > 0:
+#             data[str(k)] = j['src']
+#     k+=1
 
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+# with open('data.json', 'w') as outfile:
+#     json.dump(data, outfile)
